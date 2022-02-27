829a3e6cdd67f42a48c6b4291338892062307ee3
tridsanu taychathunyachot <tridsanu.tay@gmail.com>
2022-02-27 21:33:35 +0700

    Repository of Cart package

---
+++ src/main/java/com/example/Shopping_online/cart/CartRepository.java
@@ -0,0 +1,11 @@
+package com.example.Shopping_online.cart;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
+    Optional<Cart> findByName(String name);
+}
